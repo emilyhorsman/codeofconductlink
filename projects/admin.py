6d886a6f5c143c2c3c743b6b95e7e6fb1ab3e8ce
Emily Horsman <me@emilyhorsman.com>
2015-06-19 13:53:56 -0400

    Extended Django auth user

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import SubmissionProfile
+
+class SubmissionProfileInline(admin.StackedInline):
+    model = SubmissionProfile
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (SubmissionProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,2 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
+class SubmissionProfile(models.Model):
+    public_name = models.CharField(max_length=256, unique=True, blank=True, null=True)
+    user        = models.OneToOneField(User)

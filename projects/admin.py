705d72c22e3b40fa83219b8efd55c3af374ddda1
Emily Horsman <me@emilyhorsman.com>
2015-06-19 15:31:46 -0400

    Added LinkSubmission and RepresentationSubmission.

--- projects/admin.py
+++ projects/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import SubmissionProfile, Project, ProjectSubmission
+from .models import SubmissionProfile, Project, ProjectSubmission, LinkSubmission, RepresentationSubmission
 
 admin.site.unregister(User)
 
@@ -25,4 +25,12 @@ class ProjectAdmin(admin.ModelAdmin):
 
 @admin.register(ProjectSubmission)
 class ProjectSubmissionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'project', 'name', 'homepage', 'tags', 'is_verified', 'created_date')
+    list_display = ('id', 'user', 'project', 'name', 'homepage', 'tags', 'verified_date', 'created_date')
+
+@admin.register(LinkSubmission)
+class LinkSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'project', 'tag', 'url', 'project_has_tag', 'verified_date', 'created_date')
+
+@admin.register(RepresentationSubmission)
+class RepresentationSubmission(admin.ModelAdmin):
+    list_display = ('id', 'user', 'project', 'tag', 'verified_date', 'created_date')
--- projects/models.py
+++ projects/models.py
@@ -39,17 +39,14 @@ class Project(models.Model):
         return 'No name given.'
 
 class ProjectSubmission(models.Model):
-    user          = models.ForeignKey(User, related_name='submitted_by')
+    user          = models.ForeignKey(User, related_name='project_submissions')
     project       = models.ForeignKey(Project, related_name='submissions')
     created_date  = models.DateTimeField(default=timezone.now)
     verified_date = models.DateTimeField(blank=True, null=True)
-    verified_by   = models.ForeignKey(User, blank=True, null=True, related_name='verified_by')
+    verified_by   = models.ForeignKey(User, blank=True, null=True, related_name='project_verifications')
     name          = models.CharField(max_length=256, null=True, blank=True)
     homepage      = models.CharField(max_length=256, null=True, blank=True)
     tags          = models.CharField(max_length=256, null=True, blank=True)
-
-    def is_verified(self):
-        return bool(self.verified_date)
 
     def verify(self, verifying_user):
         self.verified_date = timezone.now()
@@ -58,3 +55,41 @@ class ProjectSubmission(models.Model):
 
     def __str__(self):
         return 'Name: {} Homepage: {} Tags: {}'.format(self.name, self.homepage, self.tags)
+
+class LinkSubmission(models.Model):
+    TAGS = (
+        ('COC', 'Code of Conduct'),
+        ('DIV', 'Diversity Statement'),
+        ('PRC', 'Problematic Conduct'),
+    )
+    user            = models.ForeignKey(User, related_name='link_submissions')
+    project         = models.ForeignKey(Project, related_name='link_submissions')
+    tag             = models.CharField(max_length=3, choices=TAGS)
+    created_date    = models.DateTimeField(default=timezone.now)
+    verified_date   = models.DateTimeField(blank=True, null=True)
+    verified_by     = models.ForeignKey(User, blank=True, null=True, related_name='link_verifications')
+    url             = models.CharField(max_length=256, blank=True, null=True)
+    # A LinkSubmission could be made with no URL and project_has_tag set to
+    # False. e.g. Project does not have a Code of Conduct, verified on...by...
+    project_has_tag = models.BooleanField(default=False)
+
+class RepresentationSubmission(models.Model):
+    # The purpose of this field is to describe contributors on the project. For
+    # instance, if a project has openly queer contributors it could be tagged
+    # queer. We can have icons for particular tags such as queer, trans, woman,
+    # fem, PoC, etc
+    tag             = models.CharField(max_length=256)
+    # A public message will be displayed on the project page history. It can
+    # display a link to the team member's work, etc.
+    public_message  = models.TextField(blank=True, null=True)
+    # A private message will be displayed only to moderators involved with
+    # submission verification. This can be a form of proof or a way to contact
+    # the representing member. Representation submissions that contain
+    # identifying information will only be verified if the member can be
+    # contacted.
+    private_message = models.TextField(blank=True, null=True)
+    project         = models.ForeignKey(Project, related_name='representation_submissions')
+    user            = models.ForeignKey(User, related_name='representation_submissions')
+    created_date    = models.DateTimeField(default=timezone.now)
+    verified_date   = models.DateTimeField(blank=True, null=True)
+    verified_by     = models.ForeignKey(User, blank=True, null=True, related_name='representation_verifications')

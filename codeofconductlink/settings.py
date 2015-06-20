d5385d5b812a61c6c3d7da3bd90ac77e4775576c
Emily Horsman <me@emilyhorsman.com>
2015-06-20 11:47:32 -0400

    wait this is easier

--- codeofconductlink/settings.py
+++ codeofconductlink/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.environ.get('STATICFILES_DIR'),)
 STATIC_URL = '/static/' # https://foo.org/static/
 
 LOGIN_REDIRECT_URL = '/'

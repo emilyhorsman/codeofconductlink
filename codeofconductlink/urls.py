b80bc7e25ac2287f162914566e81f6874428f227
Emily Horsman <me@emilyhorsman.com>
2015-06-21 14:05:59 -0400

    allauth styling work.

--- codeofconductlink/urls.py
+++ codeofconductlink/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^profiles/', include('profiles.urls', namespace='profiles')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
---
+++ profiles/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from . import views
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='detail'),
+]

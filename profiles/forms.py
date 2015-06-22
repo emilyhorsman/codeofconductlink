b7053ba9206e20ebd10d712d10e79d4c1b869fde
Emily Horsman <me@emilyhorsman.com>
2015-06-22 12:48:09 -0400

    crispy update profile

---
+++ profiles/forms.py
@@ -0,0 +1,7 @@
+from projects.forms import create_crispy_model_form
+from .models import Profile
+
+class ProfileDetailForm(create_crispy_model_form(legend_text='Update Profile')):
+    class Meta:
+        model = Profile
+        fields = ('email', 'public_name', 'profile_slug',)
--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,15 @@
 from django.views.generic.edit import UpdateView
 from braces.views import FormValidMessageMixin
 from .access_mixins import VerifiedEmailRequiredMixin
+from .forms import ProfileDetailForm
 from .models import Profile
 
 class ProfileDetail(FormValidMessageMixin,
                     VerifiedEmailRequiredMixin,
                     UpdateView):
     model  = Profile
-    fields = ('email', 'public_name', 'profile_slug',)
     template_name     = 'profiles/profile_detail.html'
+    form_class = ProfileDetailForm
     form_valid_message = 'Profile updated!'
 
     def get_object(self):

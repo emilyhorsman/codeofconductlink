62de1c96b77a48e85e1924fe5e9042c7b22392a9
Emily Horsman <me@emilyhorsman.com>
2015-06-22 18:21:44 -0400

    Fixed bug for form generator

--- projects/forms.py
+++ projects/forms.py
@@ -4,18 +4,18 @@ from crispy_forms.layout import Submit, Layout, Fieldset
 from profiles.recaptcha import ReCAPTCHAField
 from .models import Project, Report
 
-def create_crispy_model_form(**kwargs):
+def create_crispy_model_form(**form_kwargs):
     class BaseCrispyForm(models.ModelForm):
         def __init__(self, *args, **kwargs):
             super(BaseCrispyForm, self).__init__(*args, **kwargs)
             self.helper = FormHelper()
             self.helper.layout = Layout(
-                Fieldset(kwargs.get('legend_text', ''), *self.Meta.fields),
-                Submit('submit', kwargs.get('submit_text', 'Submit'), css_class='pure-button pure-button-primary')
+                Fieldset(form_kwargs.get('legend_text', ''), *self.Meta.fields),
+                Submit('submit', form_kwargs.get('submit_text', 'Submit'), css_class='pure-button pure-button-primary')
             )
             self.helper.form_class = 'pure-form pure-form-stacked'
             self.helper.form_method = 'post'
-            self.helper.form_action = kwargs.get('action', '')
+            self.helper.form_action = form_kwargs.get('action', '')
 
     return BaseCrispyForm
 

0e5b24335d617caf9b805c2cdcced84720989feb
Emily Horsman <me@emilyhorsman.com>
2015-06-23 23:55:14 -0400

    truncated long homepage URLs

--- common/templatetags/url_extras.py
+++ common/templatetags/url_extras.py
@@ -1,8 +1,16 @@
 from django import template
 from urllib.parse import urlparse
 
+register = template.Library()
+
+@register.filter(name='netloc')
 def netloc(value):
     return '.'.join(urlparse(value).netloc.split('.')[-2:])
 
-register = template.Library()
-register.filter('netloc', netloc)
+@register.filter(name='truncatewith')
+def truncatewith(text, args):
+    length, replacement = args.split(",")
+    length = int(length)
+    if len(text) <= length:
+        return text
+    return replacement.strip()

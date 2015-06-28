cba427a1546ff5872c15a36c84737dbda7f7a263
Emily Horsman <me@emilyhorsman.com>
2015-06-28 14:23:13 -0400

    Vanilla views for profiles.

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import UpdateView
+from vanilla import UpdateView
 from braces.views import FormValidMessageMixin
 from common.access_mixins import VerifiedEmailRequiredMixin
 from .forms import ProfileDetailForm
@@ -7,9 +7,9 @@ from .models import Profile
 class ProfileDetail(FormValidMessageMixin,
                     VerifiedEmailRequiredMixin,
                     UpdateView):
-    model  = Profile
-    template_name     = 'profiles/profile_detail.html'
-    form_class = ProfileDetailForm
+    model              = Profile
+    template_name      = 'profiles/profile_detail.html'
+    form_class         = ProfileDetailForm
     form_valid_message = 'Profile updated!'
 
     def get_object(self):

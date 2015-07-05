6dcd45589e331e20602bc3bee1f85661150e4eb0
Emily Horsman <me@emilyhorsman.com>
2015-07-05 17:08:26 -0400

    Upgraded material design icons to 2.1.1. Updated README accordingly for new class format. Updated icon classes. Fixed indendtation in JS. Removed link icons from project toolbar and made them a hover state instead, created generic JS to do so.

--- common/templatetags/icons.py
+++ common/templatetags/icons.py
@@ -5,4 +5,10 @@ register = template.Library()
 
 @register.simple_tag
 def icon(css_class):
-    return """<i class="{}" aria-hidden="true" role="presentation"></i>""".format(css_class)
+    base = '<i {} aria-hidden="true" role="presentation"></i>'
+    if ';' in css_class:
+        attrs = 'class="{}" data-hover="{}"'.format(*css_class.split(';'))
+    else:
+        attrs = 'class="{}"'.format(css_class)
+
+    return base.format(attrs)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,9 +1,22 @@
 $(function () {
-  var $messageCloseButtons = $('.message .close-button');
+    var $messageCloseButtons = $('.message .close-button');
 
-  $messageCloseButtons.css('visibility', 'visible');
+    $messageCloseButtons.css('visibility', 'visible');
 
-  $messageCloseButtons.on('click', function () {
-    $(this).parent().hide();
-  });
+    $messageCloseButtons.on('click', function () {
+        $(this).parent().hide();
+    });
+
+    var hover_icon = function() {
+        if (!$(this).data('hover'))
+            return;
+
+        var old_class = $(this).attr('class');
+        $(this).attr('class', $(this).data('hover'));
+        $(this).data('hover', old_class);
+    };
+
+    $('a[rel="external"]').hover(function() {
+        hover_icon.call($(this).find('[data-hover]'));
+    });
 });
